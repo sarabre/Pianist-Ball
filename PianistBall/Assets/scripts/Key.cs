8b1249ae0adfe30421caa2380a7474c60fd00023
Sara Eftekhari <sarabre26@gmail.com>
2022-04-06 02:22:14 -0700

    testing audiu and testing animation for keys

--- PianistBall/Assets/scripts/Key.cs
+++ PianistBall/Assets/scripts/Key.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    public int keyIndex;
+
     Vector3 NavigationForce = new Vector3(0,80,0);
     
     private int currentnote
@@ -23,22 +25,32 @@ public class Key : MonoBehaviour
 
     private void NavigateDistance(int currentnote, int nextnote)
     {
-        NavigationForce.x = ((currentnote - nextnote) ) *20;
-        Debug.Log("NavigationForce  =  " + NavigationForce);
+        NavigationForce.x = ((currentnote - nextnote) ) *19.5f;
+        //Debug.Log("NavigationForce  =  " + NavigationForce);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("ball"))
         {
-            Debug.Log("run this sheet");
+          //  Debug.Log("run this shit");
             NavigateDistance(Manager.Instance.levelManager.SongIndex(currentnote), Manager.Instance.levelManager.SongIndex(currentnote + 1));
             collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
             collision.gameObject.GetComponent<Rigidbody>().AddForce(NavigationForce);
-            
+            Manager.Instance.audioManager.PlaySound(keyIndex);
+          //  gameObject.GetComponent<Animator>().con();
+           // StartCoroutine("DoAnimation");
             currentnote++;
         }
     }
 
+     IEnumerator DoAnimation()
+    {
+        
+        Debug.Log("before wait");
+        yield return new WaitForSeconds(.5f);
+        Debug.Log("after wait");
+        
+    }
     
 }
